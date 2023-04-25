026f1d50c575a11c39bee7d293c32604508a07c2
John Kinya <kingjohnkn@gmail.com>
2023-04-25 18:05:03 +0300

    update to git

---
+++ class_work/ex1.py
@@ -0,0 +1,9 @@
+first_name = 'Alex'
+second_name = 'Mwenda'
+age = '32'
+profession = 'farmer'
+
+description = 'My name is ' + first_name + ' ' + second_name \
+    + '. I am ' + age + ' years old and I am a ' + profession + '.'
+
+print(description)
